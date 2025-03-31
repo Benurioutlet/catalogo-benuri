ce3ba01d9b916553a76fc1a35f61560c7eaea6d9
Benurioutlet <admin.financeiro@benurioutlet.com>
2025-03-31 14:57:20 -0300

    atualizar-imagem

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -18,6 +18,7 @@ import logo10 from './image/canto_malta - Copia.png';
 import logo11 from './image/mesa_liz.png';
 import logo12 from './image/isis_6cadeiras.png';
 import logo13 from './image/isis4cadeiras.png';
+import logo14 from './image/buffet_Iaris_off.png';
 
 import { useState } from "react";
 
@@ -38,7 +39,8 @@ export const AppRoutes = () => {
         { id: 9, name: "Cadeira Individual ", price: "350,00", codigo: "1795", category: "Sala de Jantar", observacao: "Verificar  cor das cadeiras com vendedor.Trabalhamos com encomendas", image: logo7 },
         { id: 10, name: "Cadeira Individual ", price: "300,00", codigo: "1796", category: "Sala de Jantar", observacao: "Verificar cor das cadeiras com vendedor.Trabalhamos com encomendas", image: logo8 },
         { id: 11, name: "Cadeira Individual ", price: "350,00", codigo: "1797", category: "Sala de Jantar", observacao: "Verificar cor das cadeiras com vendedor.Trabalhamos com encomendas", image: logo9 },
-      
+        { id: 16, name: "Buffet Com Adega - Off White/Freijo ", price: "655,00", codigo: "316", category: "Sala de Jantar", observacao: "Verificar modelo e cor das cadeiras com vendedor.Trabalhamos com encomendas", image: logo14 },
+
       
     ]);
 
